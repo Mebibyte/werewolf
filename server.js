ce2cd3a002083511b8758095aa86c55970b0ae5d
Glenn Hollingsworth <duelgamer@gmail.com>
2014-10-21 02:11:52 -0400

    Added nickname support

--- server.js
+++ server.js
@@ -11,15 +11,19 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 1337;
 
 app.get('/', function(req, res){
-  res.sendFile('index.html', {root: __dirname});
+    res.sendFile('index.html', {root: __dirname});
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+    socket.nickname = "Guest" + Object.keys(io.sockets.connected).length;
+    socket.on('chat message', function(msg){
+        io.emit('chat message', socket.nickname + ": " + msg);
+    });
+    socket.on('set nickname', function (name) {
+        socket.nickname = name;
+    });
 });
 
 http.listen(port, function(){
-  console.log('listening on *:' + port);
+    //console.log('listening on *:' + port);
 });
