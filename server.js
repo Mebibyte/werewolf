ad14a1808e26e2987788b18b1a4af49167c6aaac
Glenn Hollingsworth <duelgamer@gmail.com>
2014-10-21 01:11:33 -0400

    chat example

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
-var http = require('http')
+/*var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World\n');
-}).listen(port);
+}).listen(port);*/
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 1337;
+
+app.get('/', function(req, res){
+  res.sendFile('index.html', {root: __dirname});
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
