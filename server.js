5186035fb910d1986bce6ea65bbe5145f5bef0a9
Glenn Hollingsworth <duelgamer@gmail.com>
2014-10-27 03:52:06 -0400

    Change to just names

--- server.js
+++ server.js
@@ -1,10 +1,3 @@
-/*var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);*/
-
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -25,5 +18,5 @@ io.on('connection', function(socket){
 });
 
 http.listen(port, function(){
-    //console.log('listening on *:' + port);
+    console.log('listening on *:' + port);
 });
