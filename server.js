c37b251feb1cc22d6a13510b2c5100aa399b90f9
Glenn Hollingsworth <duelgamer@gmail.com>
2014-10-27 03:57:03 -0400

    revert2

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+/*var http = require('http')
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);*/
+
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -14,6 +21,5 @@ io.on('connection', function(socket){
 });
 
 http.listen(port, function(){
-  console.log('listening on *:' + port);
   console.log('listening on *:' + port);
 });
